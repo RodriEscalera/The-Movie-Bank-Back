0cf18962a5fa321cb63b20e4d6c2a573132f9843
Rodriix99 <roescal347@gmail.com>
2022-12-22 15:39:49 -0300

    cors

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:3000",
+    origin: "https://the-movie-bank.vercel.app",
     methods: ["GET", "POST", "DELETE", "OPTIONS"],
   })
 );
