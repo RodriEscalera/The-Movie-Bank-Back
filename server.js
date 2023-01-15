d12f15f842fb3dea38b6e6215953632fdcc5b01a
Rodriix99 <roescal347@gmail.com>
2023-01-15 19:38:45 -0300

    permission

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const routes = require("./routes");
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://the-movie-bank.vercel.app",
     credentials: true,
   })
 );
